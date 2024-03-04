9606b1d7f71f5465c93ac797c97fbfaa28d07351
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2024-03-04 19:49:31 +0700

    Start

---
+++ app/src/main/java/com/fajri/bangkit_2024_fp_bmaaup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fajri.bangkit_2024_fp_bmaaup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bangkit-2024-FP-BMAAUP"
+include(":app")
+ 
